63f8b540e1f36b925e03759bd2dd115f063c6194
daichi_yamaoka <tnychor06@gmail.com>
2022-09-20 16:56:12 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Webpack設定
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.(glsl|vert|frag)$/,
+  //     loader: 'ts-shader-loader',
+  //   })
+  //   return config
+  // },
 }
 
 module.exports = nextConfig
